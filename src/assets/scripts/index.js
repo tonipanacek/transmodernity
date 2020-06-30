0d30e9a67239cd6d432cf80c39a99f31f5c8b80a
Toni Panacek <toni.panacek@gmail.com>
2020-06-30 16:53:27 +0200

    removed js script for localstorage oops

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,18 +1,18 @@
 const foo = (() => Promise.resolve().then(() => console.log('Hello world')))();
 
-const initializeIntroVar = (() => {
-  Promise.resolve()
-  .then(() => {
-    if (window.location.pathname === '/' && !localStorage.getItem('intro')) {
-      localStorage.setItem('intro', 'false');
-      window.location.replace('/intro')
-    } else {
-      console.log('hi toni')
-    }
-  })
-})();
+// const initializeIntroVar = (() => {
+//   Promise.resolve()
+//   .then(() => {
+//     if (window.location.pathname === '/' && !localStorage.getItem('intro')) {
+//       localStorage.setItem('intro', 'false');
+//       window.location.replace('/intro')
+//     } else {
+//       console.log('hi toni')
+//     }
+//   })
+// })();
 
 
-const checkIntroVar = (() => {
+// const checkIntroVar = (() => {
 
-})
+// })
