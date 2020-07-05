0810f52522d178b1894fe287c13f3caae862982c
Toni Panacek <toni.panacek@gmail.com>
2020-07-06 00:12:29 +0200

    fixed the image card issue

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,1 +1,8 @@
-const foo = (() => Promise.resolve().then(() => console.log('Hello world')))();
+// const foo = (() => Promise.resolve().then(() => console.log('Hello world')))();
+
+const hamburger = document.querySelector('.nav-toggle');
+const menu = document.querySelector('.main-nav');
+hamburger.addEventListener('click', event => {
+  menu.style.display = 'flex';
+});
+
